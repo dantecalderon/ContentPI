f92b72b239462000d0cb566abc860a329dbe08a3
Carlos Santana <carlos.santana@dev.education>
2020-03-29 23:01:59 -0700

    V8 - Login UI

---
+++ frontend/src/app/users/components/Login/Layout.tsx
@@ -0,0 +1,22 @@
+// Dependencies
+import React, { FC, ReactElement } from 'react'
+import Head from 'next/head'
+
+// Components
+import Login from './Login'
+
+// Styles
+import './Layout.scss'
+
+const Layout: FC = (): ReactElement => (
+  <>
+    <Head>
+      <title>Login</title>
+      <meta name="title" content="Login" />
+    </Head>
+
+    <Login />
+  </>
+)
+
+export default Layout
