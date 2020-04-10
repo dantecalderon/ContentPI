ecc96c3351ba45210115f20ed0d23767bd5ec087
Carlos Santana <carlos.santana@dev.education>
2020-04-09 21:47:30 -0700

    Fixing space

--- frontend/src/contexts/app.tsx
+++ frontend/src/contexts/app.tsx
@@ -36,6 +36,7 @@ const AppProvider: FC<iProps> = ({ children }): ReactElement => {
         setState({
           [queryName]: data[queryName]
         })
+
         return data
       }
     } catch (err) {
